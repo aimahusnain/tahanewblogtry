ffe9982be429d2e5fea7d2e5c69f4273e6baa310
aimahusnain <aimahusnain@gmail.com>
2024-01-09 21:02:02 +0500

    jnh

--- src/database/index.tsx
+++ src/database/index.tsx
@@ -1,9 +1,5 @@
+// database.ts
+// database.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma = globalPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalPrisma.prisma = prisma;
-
-export default prisma;
+export const prisma = new PrismaClient();
